4aae7e45bcb1d8df8207a3564638cc246c51cb3d
Lyvia COURLA <lyvia_courla@hotmail.com>
2023-11-03 13:33:37 +0100

    finito pipo

--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,14 @@ const slides = [
 	}
 ]
 
+
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
+const imageBanner = document.querySelector(".banner-img");
+const text = document.querySelector('p');
+const conteneurDots = document.querySelector(".dots");
 
+let counter = 0;
 // Vérification que la flèche gauche est interactive
 arrowLeft.addEventListener("click", () => {
 	console.log("Je clique à gauche");
@@ -28,29 +33,90 @@ arrowLeft.addEventListener("click", () => {
 // Vérification que la flèche droite est interactive
 arrowRight.addEventListener("click", () => {
 	alert("Je clique à droite");
-})
+	// if (counter == slides.length -1) {
+	// 	counter = 0;
+	// } else {
+	// 	counter = counter + 1;
+	// }
+	// console.log(counter)
+	
+	
+});
 
-//_______________Déclaration et ajout des points__________________
 
-// Initialisation du slide actuelle a 0
-let numeroDot = 0;
+//_______________Déclaration et ajout des points sur la diapo__________________
 
-// La classe .dots est selectionne grace a la creation de la variable conteneurDots
-let conteneurDots = document.querySelector(".dots");
+// Initialisation de l'index de la diapo
+let indexDiapo = 0;
+
 
 // tant que i est inférieur à la totalité des bulles, on ajoute 1 à i
 for (let i = 0; i < slides.length; i++) {
-
-	// création d'une nouvelle div
+	
+	// création d'une nouvelle div pour les points non selectionnés
 	let blackDot = document.createElement("div");
 
-	// Cette nouvelle div correspond à la classe .dot
+	// Cette nouvelle div correspond à la classe .dot qui s'affiche en mode list
 	blackDot.classList.add("dot");
 
 	// La nouvelle div est l'enfant de .conteneurDots
 	conteneurDots.appendChild(blackDot);
 
+	if (i == indexDiapo) {
+		blackDot.classList.add("dot_selected");	
+	}
 
-if (i === numeroDot)blackDot.classList.add("dot_selected");	
 }
 
+// _____________Changement des points sélectionnés en fonction de l'index de la dapo_________________________ 
+
+function nextDotSelected() {
+
+	let totalDots = document.querySelectorAll(".dot");
+
+	// for (let i = 0; i < totalDots.length; i++){
+		
+	// 	totalDots[i].classList.remove("dot_selected");
+		
+	// 	if (i == indexDiapo){
+	// 		totalDots[i].classList.add("dot_selected");
+	// 	}
+	// }
+
+	for (let i = 0; i < totalDots.length; i++){
+
+	if (i == indexDiapo){
+		totalDots[i].classList.add("dot_selected");
+	}else {
+		totalDots[i].classList.remove("dot_selected");
+	}
+}
+}
+
+// _____________Changement de l'image de la diapo et du texte en fontion de l'index de le diapo ______________
+
+arrowRight.addEventListener("click", () => {
+	if (indexDiapo == slides.length - 1){
+			indexDiapo = 0;
+	} else {
+		indexDiapo = indexDiapo + 1;
+	}
+	imageBanner.src = "assets/images/slideshow/" + slides[indexDiapo].image;
+	text.innerHTML = slides[indexDiapo].tagLine;
+
+//appelle de la fonction pour que le changement de points se fasse
+	nextDotSelected();
+}
+);
+
+arrowLeft.addEventListener("click",() =>{
+	if (indexDiapo == 0) {
+		indexDiapo = slides.length - 1;
+	} else {
+		indexDiapo = indexDiapo - 1;
+	}
+	imageBanner.src = "assets/images/slideshow/" + slides[indexDiapo].image;
+	text.innerHTML = slides[indexDiapo].tagLine;
+	
+	nextDotSelected();
+})
